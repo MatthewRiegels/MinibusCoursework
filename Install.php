affd80b9db289b18145ab6afc34a9fe934187691
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-04-17 11:00:58 +0100

    set up basic php pages

---
+++ Install.php
@@ -0,0 +1,43 @@
+<?php
+include_once("connection.php");
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblVehicles;
+CREATE TABLE TblVehicles (
+    VehicleID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    RegNumber VARCHAR(7) NOT NULL,
+    Capacity INT(2) UNSIGNED NOT NULL,
+    NotAvailableFrom DATE
+);");
+$stmt->execute();
+$stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUsers;
+CREATE TABLE TblUsers (
+    UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Password VARCHAR(60) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    TelephoneNumber Varchar(11) NOT NULL,
+    Forename VARCHAR(30) NOT NULL,
+    Surname VARCHAR(30) NOT NULL,
+    IsDriver TINYINT(1) NOT NULL,
+    IsAdmin TINYINT(1) NOT NULL,
+    HoursWorked INT(3) UNSIGNED
+);");
+$stmt->execute();
+$stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblRequests;
+CREATE TABLE TblRequests (
+    RequestID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    DateTimeOut DATETIME NOT NULL,
+    TimeIn TIMESTAMP NOT NULL,
+    Destination VARCHAR(20) NOT NULL,
+    Postcode VARCHAR(8) NOT NULL,
+    Purpose VARCHAR(20) NOT NULL,
+    ReqCapacity INT(2) NOT NULL,
+    DriverID INT(6),
+    VehicleID INT(6),
+    RequestorID INT(6)
+);");
+$stmt->execute();
+$stmt->closeCursor();
