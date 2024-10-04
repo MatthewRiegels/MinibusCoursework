24652b86d0cc5295df2b646260d1f2219dc580c3
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-10-04 09:46:04 +0100

    .

--- submission_action.php
+++ submission_action.php
@@ -1,5 +1,13 @@
 <?php
 include_once('connection.php');
 
-echo $_POST
+// displaying all values for testing purposes
+echo("DateOfJob: " . $_POST['DateOfJob'] . "<br>");
+echo("TimeOut: " . $_POST['TimeOut'] . "<br>");
+echo("TimeIn: " . $_POST['TimeIn'] . "<br>");
+echo("Destination: " . $_POST['Destination'] . "<br>");
+echo("Postcode: " . $_POST['Postcode'] . "<br>");
+echo("RequiredCapacity: " . $_POST['RequiredCapacity'] . "<br>");
+echo("Purpose: " . $_POST['Purpose'] . "<br>");
+echo("DriverRequired: " . $_POST['DriverRequired'] . "<br>");
 ?>
