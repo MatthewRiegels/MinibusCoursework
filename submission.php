474c67aab29124ae67c08791e19c95290f67595e
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-10-15 20:34:09 +0100

    .

--- submission.php
+++ submission.php
@@ -6,17 +6,17 @@
     <body>
         <h1>Submit a Request</h1>
         <!-- form for adding requests -->
-        <form action="submission_action.php" method = "post">
+        <form action='submission_action.php' method = 'post'>
             <!--  code to ensure DateOfJob is in future -->
             <?php echo('Date of Job:<input type="date" name="DateOfJob", min="' . date("Y-m-d") . '"><br>'); ?>
-            Time out:<input type="time" name="TimeOut"><br>
-            Time in:<input type="time" name="TimeIn"><br>
-            Destination:<input type="text" name="Destination"><br>
-            Postcode:<input type="text" name="Postcode" minlength="5" maxlength="8"><br>
-            Required Capacity:<input type="number" name="RequiredCapacity" min="1"><br>
-            Purpose:<input type="text" name="Purpose"><br>
-            Driver Required?<input type="checkbox" name="DriverRequired"><br>
-            <input type="submit" value="Submit Request">
+            Time out:<input type='time' name='TimeOut'><br>
+            Time in:<input type='time' name='TimeIn'><br>
+            Destination:<input type='text' name='Destination'><br>
+            Postcode:<input type='text' name='Postcode' minlength='5' maxlength='8'><br>
+            Required Capacity:<input type='number' name='RequiredCapacity' min='1'><br>
+            Purpose:<input type='text' name='Purpose'><br>
+            Driver Required?<input type='checkbox' name='DriverRequired'><br>
+            <input type='submit' value='Submit Request'>
         </form>
     </body>
 </html>
--- submission_action.php
+++ submission_action.php
@@ -2,19 +2,28 @@
 include_once('connection.php');
 
 // displaying all values for testing purposes
-echo("DateOfJob: " . $_POST['DateOfJob'] . "<br>");
-echo("TimeOut: " . $_POST['TimeOut'] . "<br>");
-echo("TimeIn: " . $_POST['TimeIn'] . "<br>");
-echo("Destination: " . $_POST['Destination'] . "<br>");
-echo("Postcode: " . $_POST['Postcode'] . "<br>");
-echo("RequiredCapacity: " . $_POST['RequiredCapacity'] . "<br>");
-echo("Purpose: " . $_POST['Purpose'] . "<br>");
-echo("DriverRequired: " . $_POST['DriverRequired'] . "<br>");
+echo('DateOfJob: ' . $_POST['DateOfJob'] . '<br>');
+echo('TimeOut: ' . $_POST['TimeOut'] . '<br>');
+echo('TimeIn: ' . $_POST['TimeIn'] . '<br>');
+echo('Destination: ' . $_POST['Destination'] . '<br>');
+echo('Postcode: ' . $_POST['Postcode'] . '<br>');
+echo('RequiredCapacity: ' . $_POST['RequiredCapacity'] . '<br>');
+echo('Purpose: ' . $_POST['Purpose'] . '<br>');
+echo('DriverRequired: ' . $_POST['DriverRequired'] . '<br>');
 
 // validation
+$valid = 'True';
 // TimeIn must be greater than TimeOut
-if ($_POST['TimeIn'] <= $_POST['TimeOut']){// idk if this works
-    $valid = False;
+if ($_POST['TimeIn'] <= $_POST['TimeOut']){
+    $valid = 'False';
+}
+// Postcode must be alphanumeric
+$postcode_stripped = str_replace(' ', '', $_POST['Postcode']);// removes dividing space
+if (!ctype_alnum($postcode_stripped)){
+    $valid = 'False';
 }
 
+// Output validity for testing purposes
+echo('<br>');
+echo('Valid: ' . $valid);
 ?>
