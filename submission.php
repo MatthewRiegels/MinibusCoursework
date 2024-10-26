0101e7ea33e6886da7e93a86c0618e7e90ed8900
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-10-26 14:51:58 +0100

    finished submission completely

--- submission.php
+++ submission.php
@@ -8,13 +8,13 @@
         <!-- form for adding requests -->
         <form action='submission_action.php' method = 'post'>
             <?php // code to ensure DateOfJob is in future
-            echo('Date of Job:<input type="date" name="DateOfJob", min="' . date('Y-m-d') . '"><br>'); ?>
-            Time out:<input type='time' name='TimeOut'><br>
-            Time in:<input type='time' name='TimeIn'><br>
-            Destination:<input type='text' name='Destination'><br>
-            Postcode:<input type='text' name='Postcode' minlength='5' maxlength='8'><br>
-            Required Capacity:<input type='number' name='RequiredCapacity' min='1'><br>
-            Purpose:<input type='text' name='Purpose'><br>
+            echo("Date of Job:<input type='date' name='DateOfJob', min='" . date('Y-m-d') . "' required><br>"); ?>
+            Time out:<input type='time' name='TimeOut' required><br>
+            Time in:<input type='time' name='TimeIn' required><br>
+            Destination:<input type='text' name='Destination' required><br>
+            Postcode:<input type='text' name='Postcode' minlength='5' maxlength='8' required><br>
+            Required Capacity:<input type='number' name='RequiredCapacity' min='1' required><br>
+            Purpose:<input type='text' name='Purpose' required><br>
             Driver Required?<input type='checkbox' name='DriverRequired'><br>
             <input type='submit' value='Submit Request'>
         </form>
--- submission_action.php
+++ submission_action.php
@@ -1,13 +1,14 @@
 <?php
 //header('Location: submission.php'); // for testing comment this line out
-
 include_once('connection.php');
+session_start();
 
-// The checkbox input will leave $_POST['DriverRequired'] empty if the box is unchecked, or hold the value 'on' if it is checked
-// These lines ensure that $_POST['DriverRequired'] exists, and is either 'on' or 'off'
-if (!isset($_POST['DriverRequired'])){
-    $_POST['DriverRequired'] = 'off';
-}
+// Form input management
+// The checkbox input will not define $_POST['DriverRequired'] if the box is unchecked, but will hold the value 'on' if it is checked
+// These lines ensure that $_POST['DriverRequired'] always exists, and is set to either 'on' or 'off'
+if (!isset($_POST['DriverRequired'])){ 
+    $_POST['DriverRequired'] = 'off'; 
+} 
 
 // Displaying all values for testing purposes
 echo('DateOfJob: ' . $_POST['DateOfJob'] . '<br>');
@@ -36,25 +37,23 @@ echo('<br>');
 echo('Valid: ' . $valid . '<br>');
 
 // Determine how to add DriverID to table
+// If driver is required, DriverID is left blank so it can be filled with the ID of a driver that accepts the job
 // If no driver is required, DriverID is ID of requestor
-// If driver is required, DriverID is left blank
 if ($_POST['DriverRequired'] == 'on'){
-    echo('<br>Driver is required');
     $driverID = null;
 }
 else{
-    // This will require use of session variable which I haven't set up yet, so I'll set it to null until I do that
-    echo('<br>Driver is not required');
-    $driverID = null;// This will be set to the user's UserID
+    $driverID = $_SESSION['UserID'];
 }
 
 // Determine how to add RequestorID to table
-// This line will be edited once I have set up the session variable
-$requestorID = null;// This will be set to the user's UserID
+$requestorID = $_SESSION['UserID'];
 
 // Add record to TblRequests
 if ($valid == true){
-    $stmt = $conn->prepare('INSERT INTO TblRequests (RequestID,DateOfJob,TimeOut,TimeIn,Destination,Postcode,Purpose,ReqCapacity,DriverID,VehicleID,RequestorID)VALUES (null,:DateOfJob,:TimeOut,:TimeIn,:Destination,:Postcode,:Purpose,:ReqCapacity,:DriverID,null,:RequestorID)');
+    $stmt = $conn->prepare('INSERT INTO TblRequests 
+                            (RequestID,DateOfJob,TimeOut,TimeIn,Destination,Postcode,Purpose,ReqCapacity,DriverID,VehicleID,RequestorID)
+                            VALUES (null,:DateOfJob,:TimeOut,:TimeIn,:Destination,:Postcode,:Purpose,:ReqCapacity,:DriverID,null,:RequestorID)');
     $stmt->bindParam(':DateOfJob', $_POST['DateOfJob']);
     $stmt->bindParam(':TimeOut', $_POST['TimeOut']);
     $stmt->bindParam(':TimeIn', $_POST['TimeIn']);
