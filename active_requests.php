17477443c37c3243c031d681eb7d57704228a077
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-10-27 12:23:12 +0000

    active requests page almost done; needs link to details page

--- active_requests.php
+++ active_requests.php
@@ -7,14 +7,35 @@ session_start();
 <html>
     <head>
         <title>Active Requests</title>
+        <style>
+            .record-container{
+                background-color: #EAECF3;
+                border: 2px solid black;
+                border-radius: 10px;
+                margin: 2px;
+                padding: 5px;
+                width: 400px;
+            }
+        </style>
     </head>
     <body>
         <?php
-        echo("<h1>" . $_SESSION['Forename'] . ' ' . $_SESSION['Surname'] . "'s Active Requests</h1>");
-        $stmt = $conn->prepare('SELECT DateOfJob, TimeOut, TimeIn, Purpose FROM TblRequests WHERE RequestorID = ' . $_SESSION['UserID']);
-        $stmt->execute();
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            echo($row['DateOfJob'] . ' ' . $row['TimeOut'] . '-' . $row['TimeIn'] . ' - ' . $row['Purpose'] . '<br>');
+        echo('<h1>' . $_SESSION['Forename'] . ' ' . $_SESSION['Surname'] . "'s Active Requests</h1>");
+
+        echo('<h2>Accepted Jobs</h2>');
+        $stmt1 = $conn->prepare('SELECT DateOfJob, TimeOut, TimeIn, Purpose FROM TblRequests
+                                 WHERE RequestorID = ' . $_SESSION['UserID'] . ' AND DriverID IS NOT NULL AND VehicleID IS NOT NULL');
+        $stmt1->execute();
+        while ($row = $stmt1->fetch(PDO::FETCH_ASSOC)){
+            echo('<div class="record-container"><b>' . $row['DateOfJob'] . '</b> <i>' . $row['TimeOut'] . '-' . $row['TimeIn'] . '</i> - ' . $row['Purpose'] . '</div>');
+        }
+
+        echo('<h2>Pending Jobs</h2>');
+        $stmt2 = $conn->prepare('SELECT DateOfJob, TimeOut, TimeIn, Purpose FROM TblRequests
+                                 WHERE RequestorID = ' . $_SESSION['UserID'] . ' AND ( DriverID IS NULL OR VehicleID IS NULL )');
+        $stmt2->execute();
+        while ($row = $stmt2->fetch(PDO::FETCH_ASSOC)){
+            echo('<div class="record-container"><b>' . $row['DateOfJob'] . '</b> <i>' . $row['TimeOut'] . '-' . $row['TimeIn'] . '</i> - ' . $row['Purpose'] . '</div>');
         }
         ?>
     </body>
