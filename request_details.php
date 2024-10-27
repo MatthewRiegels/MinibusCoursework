9a3a49c6cb1240bf358fdc00980a4212581256f8
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-10-27 15:31:17 +0000

    started work on request details page atm just prints out basic info

--- request_details.php
+++ request_details.php
@@ -1,3 +1,25 @@
 <?php
-echo($_POST['chosenRequestID']);
+include_once('connection.php');
+session_start();
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Request Details</title>
+    </head>
+    <body>
+        <h1>Request Details</h1>
+        <?php
+        $stmt = $conn->prepare('SELECT * FROM TblRequests WHERE RequestID = "' . $_POST['chosenRequestID'] . '"');
+        $stmt->execute();
+        $arr = $stmt->fetch(PDO::FETCH_ASSOC);
+        echo('Date: ' . $arr['DateOfJob'] . '<br>');
+        echo('Time: ' . $arr['TimeOut'] . '-' . $arr['TimeIn'] . '<br>');
+        echo('Destination: ' . $arr['Destination'] . '<br>');
+        echo('Postcode: ' . $arr['Postcode'] . '<br>');
+        echo('Purpose: ' . $arr['Purpose'] . '<br>');
+        echo('Required Capacity: ' . $arr['ReqCapacity'] . '<br');
+        ?>
+    </body>
+</html>
