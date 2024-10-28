88bde009f59ac503d41821cc23c714de319731c4
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-10-28 14:04:54 +0000

    work on request details

--- request_details.php
+++ request_details.php
@@ -11,15 +11,56 @@ session_start();
     <body>
         <h1>Request Details</h1>
         <?php
+        // Request details
         $stmt = $conn->prepare('SELECT * FROM TblRequests WHERE RequestID = "' . $_POST['chosenRequestID'] . '"');
         $stmt->execute();
-        $arr = $stmt->fetch(PDO::FETCH_ASSOC);
-        echo('Date: ' . $arr['DateOfJob'] . '<br>');
-        echo('Time: ' . $arr['TimeOut'] . '-' . $arr['TimeIn'] . '<br>');
-        echo('Destination: ' . $arr['Destination'] . '<br>');
-        echo('Postcode: ' . $arr['Postcode'] . '<br>');
-        echo('Purpose: ' . $arr['Purpose'] . '<br>');
-        echo('Required Capacity: ' . $arr['ReqCapacity'] . '<br');
+        $requestArr = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->closeCursor();
+        echo('Date: ' . $requestArr['DateOfJob'] . '<br>');
+        echo('Time: ' . $requestArr['TimeOut'] . '-' . $requestArr['TimeIn'] . '<br>');
+        echo('Destination: ' . $requestArr['Destination'] . '<br>');
+        echo('Postcode: ' . $requestArr['Postcode'] . '<br>');
+        echo('Purpose: ' . $requestArr['Purpose'] . '<br>');
+        echo('Required Capacity: ' . $requestArr['ReqCapacity'] . '<br>');
+
+        // Requestor information
+        $stmt = $conn->prepare('SELECT * FROM TblUsers WHERE UserID = "' . $requestArr['RequestorID'] . '"');
+        $stmt->execute();
+        $requestorArr = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->closeCursor();
+        echo('<br><b>Requested by ' . $requestorArr['Forename'] . ' ' . $requestorArr['Surname'] . '</b><br>');
+        echo('Email: ' . $requestorArr['Email'] . '<br>');
+        echo('Telephone: ' . $requestorArr['TelephoneNumber'] . '<br>');
+
+        // Driver information
+        if ($requestArr['DriverID'] == null){
+            echo('<br><b>Pending driver assignment</b><br>');
+        }
+        elseif ($requestArr['DriverID'] == $requestArr['RequestorID']){
+            echo('<br><b>No driver required</b><br>');
+        }
+        else{
+            $stmt = $conn->prepare('SELECT * FROM TblUsers WHERE UserID = "' . $requestArr['DriverID'] . '"');
+            $stmt->execute();
+            $driverArr = $stmt->fetch(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+            echo('<br><b>Driven by ' . $driverArr['Forename'] . ' ' . $driverArr['Surname'] . '</b><br>');
+            echo('Email: ' . $driverArr['Email'] . '<br>');
+            echo('Telephone: ' . $driverArr['TelephoneNumber'] . '<br>');
+        }
+
+        // Vehicle information
+        if ($requestArr['VehicleID'] == null){
+            echo('<br><b>Pending vehicle assignment</b><br>');
+        }
+        else{
+            $stmt = $conn->prepare('SELECT * FROM TblVehicles WHERE VehicleID = "' . $requestArr['VehicleID'] . '"');
+            $stmt->execute();
+            $vehicleArr = $stmt->fetch(PDO::FETCH_ASSOC);
+            $stmt->closeCursor();
+            echo('<br><b>Vehicle assigned:</b><br>');
+            echo('Reg: ' . $vehicleArr['RegNumber'] . '<br>');
+        }
         ?>
     </body>
 </html>
