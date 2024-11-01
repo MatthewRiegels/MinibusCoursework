35e28c572f379b56f28ca0b043d5cf5afd13c435
matthewriegels <riegels.mj@oundleschool.org.uk>
2024-11-01 15:53:05 -0100

    Install.php: added test data, added TblDeclinedDrivers

--- Install.php
+++ Install.php
@@ -49,3 +49,43 @@ CREATE TABLE TblRequests (
 );");
 $stmt->execute();
 $stmt->closeCursor();
+
+// TblDeclinedDrivers
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblDeclinedDrivers;
+CREATE TABLE TblDeclinedDrivers (
+    DriverID INT(4) UNSIGNED NOT NULL,
+    RequestID INT(4) UNSIGNED NOT NULL
+);");
+$stmt->execute();
+$stmt->closeCursor();
+
+// Adding test data to the database
+
+// TblVehicles
+$stmt = $conn->prepare("INSERT INTO TblVehicles (RegNumber, Capacity, NotAvailableFrom) VALUES
+('BD15SMR', 10, '2025-11-04')
+");
+$stmt->execute();
+$stmt->closeCursor();
+
+// TblUsers
+$stmt = $conn->prepare("INSERT INTO TblUsers (Password, Email, TelephoneNumber, Forename, Surname, IsDriver, IsAdmin, IsRequestor, HoursWorked) VALUES
+('123456', 'smith.j@oundleschool.org.uk', '07305712268', 'John', 'Smith', 0, 0, 1, NULL),
+('ABCDEF', 'ferret.ro@oundleschool.org.uk', '07305724379', 'Ronald', 'Ferret', 1, 0, 0, 3),
+('P455W0RD', 'doe.j@oundleschool.org.uk', '07303836152', 'Jane', 'Doe', 0, 0, 1, NULL)
+");
+$stmt->execute();
+$stmt->closeCursor();
+
+// TblRequests
+$stmt = $conn->prepare("INSERT INTO TblRequests (DateOfJob, TimeOut, TimeIn, Destination, Postcode, Purpose, ReqCapacity, DriverID, VehicleID, RequestorID) VALUES
+('2025-05-17', '10:30', '12:40', 'Uppingham', 'LE159SE', 'Netball', 7, 2, NULL, 1),
+('2025-06-03', '12:30', '15:50', 'Old Bailey', 'EC4M 7AN', 'Quad law trip', 10, 1, NULL, 1),
+('2025-02-07', '18:00', '22:00', 'Corby Cinema', 'NN171QG', 'House cinema trip', 14, NULL, 1, 1),
+('2025-05-08', '13:30', '17:00', 'Henley', 'RG9 3DB', 'Rowing regatta', 12, 1, 1, 1),
+('2025-04-10', '14:00', '16:00', 'Bedford modern', 'MK41 7NT', 'Fives away fixture', 6, 2, NULL, 3),
+('2025-04-19', '12:00', '17:00', 'Stowe', 'MK185EH', 'Cricket away fixture', 19, 2, 1, 3)
+");
+$stmt->execute();
+$stmt->closeCursor();
+?>
